81b14cf54c216f6f1652a139ee07494ecb621e73
Wiphawan <gitwiphawan@gmail.com>
2023-03-04 03:41:13 +0700

    change detail

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
-  res.send("Phone Shop");
+  res.send("Welcome To Lovely Phone Shop");
 });
 
 module.exports = router;
